4b653a06cbc4fbe32b7e857ddcac2d22a89f2b51
Pau Oliver <pau.oliver@est.fib.upc.edu>
2016-04-17 18:58:02 +0200

    Sort conflictiveness periodically instead of keeping it ordered.

--- practica1/SAT-pau.cpp
+++ practica1/SAT-pau.cpp
@@ -21,15 +21,14 @@ uint decisionLevel;
 uint propagations, decisions;
 uint orderPositionOfNextLit;
 vector<vector<vector<int> > > clausesWhereVarIs;
-//vector<vector<int> > clausesWhereVarIsNeg;
 vector<int> orderByInfluence;  // could be vector<uint>
-uint CONFLICTS_REFRESH_DELAY = 10000;
+uint CONFLICTS_REFRESH_DELAY = 100000;
 uint conflictsSinceLastRefresh = 0;
 uint decisionsSinceLastRefresh = 0;
 vector<pair<uint, uint> > recentConflictsOrdered;
 map<uint, uint> litToConflictsVector;
-// clausesWhereVarIsPos[i] contains
-// the clauses where i appears positively
+// clausesWhereVarIsPos[i][TRUE] contains
+// the clauses where i appears with value TRUE
 
 const bool D = false;
 
@@ -38,8 +37,8 @@ clock_t tStart;
 bool influenceOrderFunction(int a, int b) {
   // TODO: don't call size(), store the size
   //     in the element 0 of the vector
-  return clausesWhereVarIs[a][TRUE].size()+clausesWhereVarIs[a][FALSE].size() >
-         clausesWhereVarIs[b][TRUE].size()+clausesWhereVarIs[b][FALSE].size();
+  return clausesWhereVarIs[a][TRUE][0]+clausesWhereVarIs[a][FALSE][0] >
+         clausesWhereVarIs[b][TRUE][0]+clausesWhereVarIs[b][FALSE][0];
 }
 
 void readClauses( ){
@@ -53,7 +52,7 @@ void readClauses( ){
   string aux;
   cin >> aux >> numVars >> numClauses;
   clauses.resize(numClauses);  
-  clausesWhereVarIs.resize(numVars+1, vector<vector<int> > (2, vector<int> (0)));
+  clausesWhereVarIs.resize(numVars+1, vector<vector<int> > (2, vector<int> (1,0)));
   //clausesWhereVarIsNeg.resize(numVars+1, vector<int>(0));
   orderByInfluence.resize(numVars+1, 0);
   recentConflictsOrdered.resize(numVars+1);
@@ -76,10 +75,10 @@ void readClauses( ){
         else clausesWhereVarIs[-lit][FALSE].push_back(i);
     }
   }
-  // for (int i = 1; i < numVars+1; ++i) {
-  //   clausesWhereVarIsNeg[i][0] = clausesWhereVarIsNeg[i].size(); 
-  //   clausesWhereVarIsPos[i][0] = clausesWhereVarIsPos[i].size(); 
-  // }
+  for (int i = 1; i < numVars+1; ++i) {
+     clausesWhereVarIs[i][FALSE][0]   = clausesWhereVarIs[i][FALSE].size(); 
+     clausesWhereVarIs[i][TRUE][0]    = clausesWhereVarIs[i][TRUE].size(); 
+  }
   sort(orderByInfluence.begin()+1, orderByInfluence.end(), influenceOrderFunction);
 }
 
@@ -97,9 +96,14 @@ void setLiteralToTrue(int lit){
   else model[-lit] = FALSE;   
 }
 
+bool recentConflictsOrderFunction(pair<uint, uint> a, pair<uint, uint> b) {
+  return a.second > b.second;
+}
+
 void updateRecentConflicts(int lit) {
   if (decisionsSinceLastRefresh > CONFLICTS_REFRESH_DELAY) {
-    int s = recentConflictsOrdered.size();
+    int s = numVars+1;
+    sort(recentConflictsOrdered.begin()+1, recentConflictsOrdered.end(), recentConflictsOrderFunction);
     for (uint i = 0; i < s; ++i) {
       if (D) cout << recentConflictsOrdered[i].first << " " << recentConflictsOrdered[i].second << ", ";
       recentConflictsOrdered[i].second /= 2;
@@ -112,6 +116,7 @@ void updateRecentConflicts(int lit) {
   uint vecPos = litToConflictsVector[lit];
   //if (conflictsSinceLastRefresh%128 == 0) cout << "vecPos: " << vecPos << endl;
   recentConflictsOrdered[vecPos].second += 20;
+  /*
   int pre = vecPos-1;
   while(pre > 0 and recentConflictsOrdered[vecPos].second > recentConflictsOrdered[pre].second) {
     pre--; // Millor ordena cada cert temps en lloc d'aixo
@@ -122,7 +127,7 @@ void updateRecentConflicts(int lit) {
     recentConflictsOrdered[vecPos] = recentConflictsOrdered[pre];
     recentConflictsOrdered[pre] = temp;
     litToConflictsVector[lit] = pre;
-  }
+  }*/
 }
 
 bool propagateGivesConflict ( ) {
@@ -141,7 +146,7 @@ bool propagateGivesConflict ( ) {
     } //else cout << " = false, ";
     //cout << "to " << clausesToPropagate.size() << " clauses." << endl;
     int s = clausesToPropagate->size();
-    for (uint i = 0; i < s; ++i) {
+    for (uint i = 1; i < s; ++i) {
       bool someLitTrue = false;
       int numUndefs = 0;
       int lastLitUndef = 0;
@@ -159,7 +164,7 @@ bool propagateGivesConflict ( ) {
       //cout << ". Last undefined lit: " << lastLitUndef << endl;
       if (not someLitTrue and numUndefs == 0) {
         // conflict! all lits false
-        //updateRecentConflicts(abs(currLit));
+        updateRecentConflicts(abs(currLit));
         return true;
       }
       else if (not someLitTrue and numUndefs == 1) setLiteralToTrue(lastLitUndef);  
@@ -167,7 +172,6 @@ bool propagateGivesConflict ( ) {
   }
   return false;
 }
-
 
 void backtrack(){
   uint i = modelStack.size() -1;
@@ -186,19 +190,17 @@ void backtrack(){
   orderPositionOfNextLit = 1;
 }
 
-
 // Heuristic for finding the next decision literal:
 int getNextDecisionLiteral(){
-  // for (uint i = 1; i <= numVars+1; ++i) {
-  //   int l = (int) recentConflictsOrdered[i].first;
-  //   if (model[l] == UNDEF) return l;
-  // }
+  for (uint i = 1; i <= numVars+1; ++i) {
+    int l = (int) recentConflictsOrdered[i].first;
+    if (model[l] == UNDEF) return l;
+  }
   //return orderByInfluence[modelStack.size()+1];
   // returns most influent UNDEF vars, positively
   for (uint i = orderPositionOfNextLit; i <= numVars+1; ++i) 
     if (model[orderByInfluence[i]] == UNDEF) return orderByInfluence[i]; 
   return 0; // reurns 0 when all literals are defined
-
 }
 
 void checkmodel(){
